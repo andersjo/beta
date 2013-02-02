b677d793536a58f311bcc27cbb154964275683b3
Marco Kuhlmann <marco.kuhlmann@lingfil.uu.se>
2013-02-02 10:05:02 +0100

    Removed the Parser interface.

--- src/beta/Main.java
+++ src/beta/Main.java
@@ -98,7 +98,7 @@ public class Main {
 
 		long trainingStarted = System.currentTimeMillis();
 
-		TrainerHandler trainerHandler = new TrainerHandler(model, new EisnerParser(model));
+		TrainerHandler trainerHandler = new TrainerHandler(model, new Parser(model));
 		for (int i = 0; i < options.nIterations; i++) {
 			System.err.format("Iteration %d of %d.%n", i + 1, options.nIterations);
 
@@ -190,7 +190,7 @@ public class Main {
 
 		long parsingStarted = System.currentTimeMillis();
 
-		ParserHandler parserHandler = new ParserHandler(new EisnerParser(model));
+		ParserHandler parserHandler = new ParserHandler(new Parser(model));
 		ProgressPrinter progressPrinter = new ProgressPrinter();
 
 		CoNLLReader reader = null;
