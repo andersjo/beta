bc506ee588ac7797507360f277034636855de297
Marco Kuhlmann <marco.kuhlmann@lingfil.uu.se>
2013-07-14 13:57:46 +0200

    Removed the PA trainer in favour of the Perceptron trainer. Simplified the interface of FeatureVector.

--- src/beta/FeatureVector.java
+++ src/beta/FeatureVector.java
@@ -3,12 +3,9 @@
  */
 package beta;
 
-import gnu.trove.iterator.TIntDoubleIterator;
 import gnu.trove.iterator.TIntIterator;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.linked.TIntLinkedList;
-import gnu.trove.map.TIntDoubleMap;
-import gnu.trove.map.hash.TIntDoubleHashMap;
 
 /**
  *
@@ -17,99 +14,44 @@ import gnu.trove.map.hash.TIntDoubleHashMap;
 public class FeatureVector {
 
 	private final static double ONE = 1.0;
-	private final TIntList pos;
-	private final TIntList neg;
+	private final TIntList features;
 
 	public FeatureVector() {
-		this.pos = new TIntLinkedList();
-		this.neg = new TIntLinkedList();
+		this.features = new TIntLinkedList();
 	}
 
 	public void increment(int index) {
-		pos.add(index);
+		features.add(index);
 	}
 
-	public void update(double alpha, double[] parameters) {
-		TIntIterator itPos = pos.iterator();
+	public void addTo(double[] parameters) {
+		addTo(ONE, parameters);
+	}
+
+	public void addTo(double alpha, double[] parameters) {
+		TIntIterator itPos = features.iterator();
 		while (itPos.hasNext()) {
 			parameters[itPos.next()] += alpha;
 		}
+	}
 
-		TIntIterator itNeg = neg.iterator();
-		while (itNeg.hasNext()) {
-			parameters[itNeg.next()] -= alpha;
+	public void subtractFrom(double[] parameters) {
+		subtractFrom(ONE, parameters);
+	}
+
+	public void subtractFrom(double alpha, double[] parameters) {
+		TIntIterator itPos = features.iterator();
+		while (itPos.hasNext()) {
+			parameters[itPos.next()] -= alpha;
 		}
 	}
 
 	public double getScore(double[] parameters) {
 		double score = 0.0;
-
-		TIntIterator itPos = pos.iterator();
+		TIntIterator itPos = features.iterator();
 		while (itPos.hasNext()) {
 			score += parameters[itPos.next()];
 		}
-
-		TIntIterator itNeg = neg.iterator();
-		while (itNeg.hasNext()) {
-			score -= parameters[itNeg.next()];
-		}
-
 		return score;
-	}
-
-	// ||x|| = \sqrt{xx}, so ||x||^2 = xx
-	public double getSquaredEuclidean() {
-		TIntDoubleMap map = getMap();
-		
-		double squaredEuclidean = 0.0;
-		
-		for (Double value : map.values()) {
-			squaredEuclidean += value * value;
-		}
-		
-		return squaredEuclidean;
-	}
-
-	public double getDotProduct(FeatureVector otherVector) {
-		TIntDoubleMap map1 = this.getMap();
-		TIntDoubleMap map2 = otherVector.getMap();
-
-		double dotProduct = 0.0;
-
-		TIntDoubleIterator it = map1.iterator();
-		while (it.hasNext()) {
-			it.advance();
-			int key = it.key();
-			if (map2.containsKey(key)) {
-				dotProduct += it.value() * map2.get(key);
-			}
-		}
-
-		return dotProduct;
-	}
-
-	private TIntDoubleMap getMap() {
-		TIntDoubleMap map = new TIntDoubleHashMap();
-
-		TIntIterator itPos = pos.iterator();
-		while (itPos.hasNext()) {
-			map.adjustOrPutValue(itPos.next(), ONE, ONE);
-		}
-
-		TIntIterator itNeg = neg.iterator();
-		while (itNeg.hasNext()) {
-			map.adjustOrPutValue(itNeg.next(), -ONE, -ONE);
-		}
-
-		return map;
-	}
-
-	public static FeatureVector getDelta(FeatureVector vector1, FeatureVector vector2) {
-		FeatureVector delta = new FeatureVector();
-		delta.pos.addAll(vector1.pos);
-		delta.neg.addAll(vector1.neg);
-		delta.neg.addAll(vector2.pos);
-		delta.pos.addAll(vector2.neg);
-		return delta;
 	}
 }
