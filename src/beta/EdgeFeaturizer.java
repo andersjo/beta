2ef4a5fb99c4366fd19c36ce5e44c9a389e770b4
Marco Kuhlmann <marco.kuhlmann@lingfil.uu.se>
2013-09-02 22:37:17 +0200

    Harmonized with gamma's Featurizer.

--- src/beta/EdgeFeaturizer.java
+++ src/beta/EdgeFeaturizer.java
@@ -15,57 +15,34 @@ public class EdgeFeaturizer {
 	private static final int END = -2;
 	private static final int MID = -3;
 	private static final int[] LIMITS = new int[]{1, 2, 3, 4, 5, 6, 11};
-	private final int[] form;
-	private final int[] postag;
-	private final int[] pred_postag;
-	private final int[] succ_postag;
-	private final int[] cpostag;
-	private final int[] pred_cpostag;
-	private final int[] succ_cpostag;
-	private final int[] lemma;
-	private final int[] pred_lemma;
-	private final int[] succ_lemma;
-	private final boolean useExtendedFeatures = false;
+	private final int[] w;
+	private final int[] t;
+	private final int[] pred_t;
+	private final int[] succ_t;
 
 	public EdgeFeaturizer(Model model, CoNLLTree tree) {
 		int nNodes = tree.getNNodes();
 
-		this.form = new int[nNodes];
-		this.lemma = new int[nNodes];
-		this.cpostag = new int[nNodes];
-		this.postag = new int[nNodes];
+		this.w = new int[nNodes];
+		this.t = new int[nNodes];
 
 		for (int i = 0; i < nNodes; i++) {
-			form[i] = model.getCodeForForm(tree.forms[i]);
-			lemma[i] = model.getCodeForForm(tree.lemmas[i]);
-			cpostag[i] = model.getCodeForPOSTag(tree.cpostags[i]);
-			postag[i] = model.getCodeForPOSTag(tree.postags[i]);
+			w[i] = model.getCodeForForm(tree.forms[i]);
+			t[i] = model.getCodeForPOSTag(tree.postags[i]);
 		}
 
-		this.pred_lemma = new int[nNodes];
-		this.pred_cpostag = new int[nNodes];
-		this.pred_postag = new int[nNodes];
+		this.pred_t = new int[nNodes];
 
-		pred_lemma[0] = BEG;
-		pred_cpostag[0] = BEG;
-		pred_postag[0] = BEG;
+		pred_t[0] = BEG;
 		for (int i = 1; i < nNodes; i++) {
-			pred_lemma[i] = lemma[i - 1];
-			pred_cpostag[i] = cpostag[i - 1];
-			pred_postag[i] = postag[i - 1];
+			pred_t[i] = t[i - 1];
 		}
 
-		this.succ_lemma = new int[nNodes];
-		this.succ_cpostag = new int[nNodes];
-		this.succ_postag = new int[nNodes];
+		this.succ_t = new int[nNodes];
 
-		succ_lemma[nNodes - 1] = END;
-		succ_cpostag[nNodes - 1] = END;
-		succ_postag[nNodes - 1] = END;
+		succ_t[nNodes - 1] = END;
 		for (int i = nNodes - 2; i >= 0; i--) {
-			succ_lemma[i] = lemma[i + 1];
-			succ_cpostag[i] = cpostag[i + 1];
-			succ_postag[i] = postag[i + 1];
+			succ_t[i] = t[i + 1];
 		}
 	}
 
@@ -98,127 +75,113 @@ public class EdgeFeaturizer {
 	}
 
 	public void featurizeCore(int fst, int snd, boolean isRA, FeatureHandler h) {
-		int suffix = makePair(isRA, quantize(snd - fst, LIMITS));
+		int attDist = makePair(isRA, quantize(snd - fst, LIMITS));
 
-		featurize1(0, postag, pred_postag, succ_postag, fst, snd, isRA, suffix, h);
-		featurize2(13, form, postag, fst, snd, isRA, suffix, h);
+		int fst_t = t[fst];
+		int snd_t = t[snd];
 
-		if (useExtendedFeatures) {
-			featurize1(33, cpostag, pred_cpostag, succ_cpostag, fst, snd, isRA, suffix, h);
-			featurize2(46, form, cpostag, fst, snd, isRA, suffix, h);
-			featurize2(59, lemma, postag, fst, snd, isRA, suffix, h);
-			featurize2(72, lemma, cpostag, fst, snd, isRA, suffix, h);
-		}
-	}
-
-	private void featurize1(int offset, int[] x, int[] pred_x, int[] succ_x, int fst, int snd, boolean isRA, int suffix, FeatureHandler h) {
-		int fst_x = x[fst];
-		int snd_x = x[snd];
-
-		int fst_pred_x = pred_x[fst];
-		int snd_succ_x = succ_x[snd];
-		int fst_succ_x = fst < snd - 1 ? succ_x[fst] : MID;
-		int snd_pred_x = snd > fst + 1 ? pred_x[snd] : MID;
+		int fst_pred_t = pred_t[fst];
+		int snd_succ_t = succ_t[snd];
+		int fst_succ_t = fst < snd - 1 ? succ_t[fst] : MID;
+		int snd_pred_t = snd > fst + 1 ? pred_t[snd] : MID;
 
 		for (int mid = fst + 1; mid < snd; mid++) {
-			int mid_t = x[mid];
-			h.handle(offset + 0, fst_x, snd_x, mid_t);
-			h.handle(offset + 0, fst_x, snd_x, mid_t, suffix);
+			int mid_t = t[mid];
+			h.handle(0, fst_t, snd_t, mid_t);
+			h.handle(0, fst_t, snd_t, mid_t, attDist);
 		}
 
-		h.handle(offset + 1, fst_pred_x, fst_x, snd_x);
-		h.handle(offset + 1, fst_pred_x, fst_x, snd_x, suffix);
+		h.handle(1, fst_pred_t, fst_t, snd_t);
+		h.handle(1, fst_pred_t, fst_t, snd_t, attDist);
 
-		h.handle(offset + 2, fst_pred_x, fst_x, snd_x, snd_succ_x);
-		h.handle(offset + 2, fst_pred_x, fst_x, snd_x, snd_succ_x, suffix);
+		h.handle(2, fst_pred_t, fst_t, snd_t, snd_succ_t);
+		h.handle(2, fst_pred_t, fst_t, snd_t, snd_succ_t, attDist);
 
-		h.handle(offset + 3, fst_pred_x, snd_x, snd_succ_x);
-		h.handle(offset + 3, fst_pred_x, snd_x, snd_succ_x, suffix);
+		h.handle(3, fst_pred_t, snd_t, snd_succ_t);
+		h.handle(3, fst_pred_t, snd_t, snd_succ_t, attDist);
 
-		h.handle(offset + 4, fst_pred_x, fst_x, snd_succ_x);
-		h.handle(offset + 4, fst_pred_x, fst_x, snd_succ_x, suffix);
+		h.handle(4, fst_pred_t, fst_t, snd_succ_t);
+		h.handle(4, fst_pred_t, fst_t, snd_succ_t, attDist);
 
-		h.handle(offset + 5, fst_x, snd_x, snd_succ_x);
-		h.handle(offset + 5, fst_x, snd_x, snd_succ_x, suffix);
+		h.handle(5, fst_t, snd_t, snd_succ_t);
+		h.handle(5, fst_t, snd_t, snd_succ_t, attDist);
 
-		h.handle(offset + 6, fst_x, fst_succ_x, snd_pred_x);
-		h.handle(offset + 6, fst_x, fst_succ_x, snd_pred_x, suffix);
+		h.handle(6, fst_t, fst_succ_t, snd_pred_t);
+		h.handle(6, fst_t, fst_succ_t, snd_pred_t, attDist);
 
-		h.handle(offset + 7, fst_x, fst_succ_x, snd_pred_x, snd_x);
-		h.handle(offset + 7, fst_x, fst_succ_x, snd_pred_x, snd_x, suffix);
+		h.handle(7, fst_t, fst_succ_t, snd_pred_t, snd_t);
+		h.handle(7, fst_t, fst_succ_t, snd_pred_t, snd_t, attDist);
 
-		h.handle(offset + 8, fst_x, fst_succ_x, snd_x);
-		h.handle(offset + 8, fst_x, fst_succ_x, snd_x, suffix);
+		h.handle(8, fst_t, fst_succ_t, snd_t);
+		h.handle(8, fst_t, fst_succ_t, snd_t, attDist);
 
-		h.handle(offset + 9, fst_x, snd_pred_x, snd_x);
-		h.handle(offset + 9, fst_x, snd_pred_x, snd_x, suffix);
+		h.handle(9, fst_t, snd_pred_t, snd_t);
+		h.handle(9, fst_t, snd_pred_t, snd_t, attDist);
 
-		h.handle(offset + 10, fst_succ_x, snd_pred_x, snd_x);
-		h.handle(offset + 10, fst_succ_x, snd_pred_x, snd_x, suffix);
+		h.handle(10, fst_succ_t, snd_pred_t, snd_t);
+		h.handle(10, fst_succ_t, snd_pred_t, snd_t, attDist);
 
-		h.handle(offset + 11, fst_pred_x, fst_x, snd_pred_x, snd_x);
-		h.handle(offset + 11, fst_pred_x, fst_x, snd_pred_x, snd_x, suffix);
+		h.handle(11, fst_pred_t, fst_t, snd_pred_t, snd_t);
+		h.handle(11, fst_pred_t, fst_t, snd_pred_t, snd_t, attDist);
 
-		h.handle(offset + 12, fst_x, fst_succ_x, snd_x, snd_succ_x);
-		h.handle(offset + 12, fst_x, fst_succ_x, snd_x, snd_succ_x, suffix);
-	}
+		h.handle(12, fst_t, fst_succ_t, snd_t, snd_succ_t);
+		h.handle(12, fst_t, fst_succ_t, snd_t, snd_succ_t, attDist);
 
-	private void featurize2(int offset, int[] a, int[] b, int fst, int snd, boolean isRA, int suffix, FeatureHandler h) {
 		int src = isRA ? fst : snd;
 		int tgt = isRA ? snd : fst;
 
-		int src_a = a[src];
-		int src_b = b[src];
-		int tgt_a = a[tgt];
-		int tgt_b = b[tgt];
+		int src_w = w[src];
+		int src_t = t[src];
+		int tgt_w = w[tgt];
+		int tgt_t = t[tgt];
 
-		h.handle(offset + 0, src_a);
-		h.handle(offset + 0, src_a, suffix);
+		h.handle(13, src_w);
+		h.handle(13, src_w, attDist);
 
-		h.handle(offset + 1, src_a, src_b);
-		h.handle(offset + 1, src_a, src_b, suffix);
+		h.handle(14, src_w, src_t);
+		h.handle(14, src_w, src_t, attDist);
 
-		h.handle(offset + 2, src_a, src_b, tgt_b);
-		h.handle(offset + 2, src_a, src_b, tgt_b, suffix);
+		h.handle(15, src_w, src_t, tgt_t);
+		h.handle(15, src_w, src_t, tgt_t, attDist);
 
-		h.handle(offset + 3, src_a, src_b, tgt_b, tgt_a);
-		h.handle(offset + 3, src_a, src_b, tgt_b, tgt_a, suffix);
+		h.handle(16, src_w, src_t, tgt_t, tgt_w);
+		h.handle(16, src_w, src_t, tgt_t, tgt_w, attDist);
 
-		h.handle(offset + 4, src_a, tgt_a);
-		h.handle(offset + 4, src_a, tgt_a, suffix);
+		h.handle(17, src_w, tgt_w);
+		h.handle(17, src_w, tgt_w, attDist);
 
-		h.handle(offset + 5, src_a, tgt_b);
-		h.handle(offset + 5, src_a, tgt_b, suffix);
+		h.handle(18, src_w, tgt_t);
+		h.handle(18, src_w, tgt_t, attDist);
 
-		h.handle(offset + 6, src_b, tgt_a);
-		h.handle(offset + 6, src_b, tgt_a, suffix);
+		h.handle(19, src_t, tgt_w);
+		h.handle(19, src_t, tgt_w, attDist);
 
-		h.handle(offset + 7, src_b, tgt_a, tgt_b);
-		h.handle(offset + 7, src_b, tgt_a, tgt_b, suffix);
+		h.handle(20, src_t, tgt_w, tgt_t);
+		h.handle(20, src_t, tgt_w, tgt_t, attDist);
 
-		h.handle(offset + 8, src_b, tgt_b);
-		h.handle(offset + 8, src_b, tgt_b, suffix);
+		h.handle(21, src_t, tgt_t);
+		h.handle(21, src_t, tgt_t, attDist);
 
-		h.handle(offset + 9, tgt_a, tgt_b);
-		h.handle(offset + 9, tgt_a, tgt_b, suffix);
+		h.handle(22, tgt_w, tgt_t);
+		h.handle(22, tgt_w, tgt_t, attDist);
 
-		h.handle(offset + 10, src_b);
-		h.handle(offset + 10, src_b, suffix);
+		h.handle(23, src_t);
+		h.handle(23, src_t, attDist);
 
-		h.handle(offset + 11, tgt_a);
-		h.handle(offset + 11, tgt_a, suffix);
+		h.handle(24, tgt_w);
+		h.handle(24, tgt_w, attDist);
 
-		h.handle(offset + 12, tgt_b);
-		h.handle(offset + 12, tgt_b, suffix);
+		h.handle(25, tgt_t);
+		h.handle(25, tgt_t, attDist);
 	}
 
 	public void featurizeLabeled(int node, int label, boolean isRA, boolean isTarget, FeatureHandler h) {
 		int suffix = makePair(isRA, isTarget);
 
-		int node_w = form[node];
-		int node_t = postag[node];
-		int node_pred_t = pred_postag[node];
-		int node_succ_t = succ_postag[node];
+		int node_w = w[node];
+		int node_t = t[node];
+		int node_pred_t = pred_t[node];
+		int node_succ_t = succ_t[node];
 
 		h.handle(26, label);
 		h.handle(26, label, suffix);
